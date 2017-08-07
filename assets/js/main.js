580cd64235fda7c4ba4b7884fb829c8c8a069241
alefranii <afmormar@gmail.com>
2017-08-07 03:16:54 -0400

    solo faltaron detalles de informacion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,5 @@
-(function menu() {
-	var lastScrollTop = 0;
-	window.addEventListener("scroll", function() {
-		var currentScroll = window.pageYOffset || document.body.scrollTop;
-		if (currentScroll > lastScrollTop) {
-			document.getElementById("nav").classList.add("bg-menu");
-		} else {
-			document.getElementById("nav").classList.add("bg-menu");
-			if ( currentScroll <= 2 ) {
-				document.getElementById("nav").classList.remove("bg-menu");
-			}
-		}
-		lastScrollTop = currentScroll;
-	}, false);
-})();
-(function menuResponsive() {
-	var btnMenu = document.getElementById('btn-menu');
-	var list = document.getElementById('list-menu');
-	btnMenu.addEventListener("click", function(event){
-		event.preventDefault();
-		list.classList.toggle('hidden-xs');
-	})	
-})();
+
+
 
 /* servicios */
 $(".hover").mouseleave(
